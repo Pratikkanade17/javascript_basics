4219a345ab836a404f8ff97ea5e65dffe95dbe5b
Pratikkanade17 <146074147+Pratikkanade17@users.noreply.github.com>
2025-12-31 15:09:57 +0000

    datatype coversation

---
+++ 01_basics/conversationOperation.js
@@ -0,0 +1,15 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//
+
+let isLoggedIn = "pratik"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
