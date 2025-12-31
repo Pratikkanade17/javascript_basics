9fee83415ac61bc1a0b9a9a76d7496f663821a10
Pratikkanade17 <146074147+Pratikkanade17@users.noreply.github.com>
2025-12-31 12:26:48 +0000

    variables and datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,8 @@
+console.log("pratik")
+let age = 18
+let isLoggedIn = false
+let state;
+
+console.log(typeof age); // it shows datatype of variable
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ 01_basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 14452
+let accountEmail = "pratik@gmail.com"
+var accountPassword = "12334"
+
+accountEmail = "pratikne@"
+accountCity = "pune"
+accountPassword = "2656626"
+let accountState 
+
+
+//accountId = 2 // not allowed
+console.log(accountId)
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
