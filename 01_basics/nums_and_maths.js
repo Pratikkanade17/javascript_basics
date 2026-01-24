87116ff39625dfe7bb6d02bad94260ed68ac45ea
Pratikkanade17 <146074147+Pratikkanade17@users.noreply.github.com>
2026-01-24 07:02:30 +0000

    nums and maths

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,31 @@
+// const val=100
+// console.log(val);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+//+++++++++++++++++++ MAths +++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.5));
+console.log(Math.min(2, 3, 3, 9));
+console.log(Math.max(2, 3, 3, 9));
+
+
+console.log(Math.random());
+// console.log(Math.random() * 10);
+console.log(Math.floor(Math.random() * 10) + 1);
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
