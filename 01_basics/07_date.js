ce637b5b2a78bd3c82cafb1c8eb1ff1fec70d3d2
Pratikkanade17 <146074147+Pratikkanade17@users.noreply.github.com>
2026-01-26 16:28:58 +0000

    date

---
+++ 01_basics/07_date.js
@@ -0,0 +1,11 @@
+// let myDate = new Date()
+// console.log(myDate);
+
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString);
+// console.log(myDate.toDateString);
+// console.log(typeof myDate);
+
+let myCreateDate = new Date(2026, 0, 26, 6, 41)
+console.log(myCreateDate); //2026-01-26T06:41:00.000Z
+console.log(myCreateDate.toLocaleString()); //1/26/2026, 6:41:00 AM
