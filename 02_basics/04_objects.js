3b4059b712846ba5aa03ebbe9db466366dca9241
Pratikkanade17 <146074147+Pratikkanade17@users.noreply.github.com>
2026-01-30 19:01:36 +0000

    object 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,52 @@
+// const newtinderUser = new object() //singleton object
+const tinderUser = {} // non-singleton object 
+
+tinderUser.email = "p14@.com"
+tinderUser.id = 123
+tinderUser.no = 18
+
+console.log(tinderUser);
+
+const regularUser = {
+    email: "p18@gmail.com" ,
+    {
+        username: {
+            fullname:
+            {
+                city: "pune",
+                id: 123
+            }
+
+        }
+    }
+}
+
+// const obj1 = {a: "1", b: "2" }
+// const obj2 = {d: "1", c: "2"}
+
+// // const obj3 = {obj1, obj2}
+// // console.log(obj3);
+
+// // const obj4 = Object.assign ({ }, obj1, obj2)
+// const obj4 = {...obj1, ...obj2}
+// console.log(obj4);
+
+// // array of object
+
+// const user = [
+//     {
+//         name : "pratik",
+//         email : "pratik01@gmail.com",
+//         no : 96645
+
+//     },
+//     {
+//         pin : 414402,
+//         city : "pune"
+//     },
+//     {
+
+//     }
+// ]
+
+// user[1].email
