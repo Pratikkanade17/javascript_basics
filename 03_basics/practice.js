964acf037349c2406e4f0bd4a675c2750fdeeb52
Pratikkanade17 <146074147+Pratikkanade17@users.noreply.github.com>
2026-02-04 15:20:43 +0000

    new practice file

---
+++ 03_basics/practice.js
@@ -0,0 +1,26 @@
+// syntax of array
+
+const a = [1,2,3]
+
+console.log(a[1]);
+
+
+// syntax of object
+
+const user = {
+    name: "",
+    id: 131,
+    pass:""
+}
+
+console.log(user.id);
+
+
+// syntax of function
+
+function add (num1, num2){
+    return num1 + num2    
+
+}
+
+console.log(add(2,3));
