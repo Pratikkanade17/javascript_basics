cae973864f43787eb618768734d351251a111de0
Pratikkanade17 <146074147+Pratikkanade17@users.noreply.github.com>
2026-02-07 19:02:10 +0000

    itorations loops

---
+++ 05_iterators/one.js
@@ -0,0 +1,26 @@
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+
+let myArray = ["flash", "batman", "superman"]
+//console.log(myArray.length);
+for (let i = 0; i < myArray.length; i++) {
+    // const element = myArray[i];
+    console.log(myArray[i]);
+}
+
+
+// for (let i = 1; i < 20; i++) {
+//     if (i==5) {
+//         console.log(`no. 5 is detected`);
+//         continue
+//     }
+
+//     console.log(`value of i is ${i}`);
+    
+// }
+
+
+
---
+++ 05_iterators/two.js
@@ -0,0 +1,16 @@
+// let i = 1;
+// while (i < 20) {
+// i++
+// console.log(i);
+    
+// }
+
+
+let i=1;
+while  (i <= 10) {
+    console.log(i);
+    i = i +1;
+    
+    
+}
+    
