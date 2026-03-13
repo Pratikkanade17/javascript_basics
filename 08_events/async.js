366c37d080e6c295994816a755f9bfe7196a25bf
Pratikkanade17 <146074147+Pratikkanade17@users.noreply.github.com>
2026-03-13 17:43:40 +0000

    new

---
+++ 08_events/async.js
@@ -0,0 +1,4 @@
+setTimeout(function() {
+    console.log("pratik");
+    
+}, 2000);
---
+++ 08_events/promises.js
@@ -0,0 +1,66 @@
+const PromiseOne= new Promise(function(resolve, reject) {
+    // Do some async tasks
+    // DB calls, Cryptography, Network calls
+    setTimeout(function(){
+        console.log("IM here");
+        resolve();
+
+    }, 2000);
+    
+})
+
+PromiseOne.then(function(){
+    console.log("Promise consumed");
+    
+})
+
+const PromiseTwo= new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        console.log("IM here");
+        resolve();
+
+    }, 2000);
+    
+}).then(function(){
+    console.log("Promise consumed");
+    
+})
+
+const PromiseThree= new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        console.log("My Details is here");
+        resolve({name: "pratik", age: 26});
+
+    }, 2000);
+    
+}).then(function(user){
+    console.log(user);
+    
+})
+
+
+const PromiseFour = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        let error = true;
+        if (!error) {
+            resolve({name: "pratik", age: 26});
+            
+        } else {
+            reject("something went wrong")
+        }
+         
+
+    }, 2000);
+    
+}).then(function(user){
+    console.log(user);
+    return user.name;
+    
+}).catch(function(error){
+    console.log(error);
+    
+}).finally(() => (console.log("the promise is either resolved or reject")
+))
+
+
+
