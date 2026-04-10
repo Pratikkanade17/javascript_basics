f12272c63b7e497587380a51bd604b0afc87614c
Pratikkanade17 <146074147+Pratikkanade17@users.noreply.github.com>
2026-04-10 18:34:19 +0000

    Dom concept

---
+++ 06_dom/dom.js
@@ -0,0 +1,10 @@
+// console.log("hello guyz");
+// console.dir(window.document)
+
+// let headings = document.getElementsByClassName("child")
+// console.dir(headings)
+// console.log(headings);
+
+
+let parah = document.getElementsByTagName("p")
+console.dir(parah)
