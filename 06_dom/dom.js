0335831914a993fc2eb00d84cf41c89dc3dd5826
Pratikkanade17 <146074147+Pratikkanade17@users.noreply.github.com>
2026-04-12 08:25:38 +0000

    DOM concepts

--- 06_dom/dom.js
+++ 06_dom/dom.js
@@ -6,5 +6,40 @@
 // console.log(headings);
 
 
-let parah = document.getElementsByTagName("p")
-console.dir(parah)
+// let parah = document.getElementsByTagName("p")
+// console.dir(parah)
+
+// let element = document.querySelector("p")
+// console.dir(element)
+
+// let element = document.querySelectorAll("p")
+// console.dir(element)
+
+// let myEle = document.querySelectorAll(".child")
+// console.dir(myEle)
+
+// console.dir(document.body.firstChild)
+
+// let div = document.querySelector("div")
+// console.dir(div)
+
+// let parah = document.querySelector("p")
+// console.dir(parah.innerText)
+
+// parah.innerText= parah.innerText + "- i have changed this paragraph" // I have concanet two strings using +
+
+let divs = document.querySelectorAll(".child")
+// console.dir(newEle)
+// console.log(newEle);
+
+// divs[0].innerText = "this is my firts box" // To chnage innertext of first box
+
+let idx = 1;
+
+for (div of divs){
+    div.innerText = `new box No. ${idx}`
+    idx++
+
+}
+ 
+
